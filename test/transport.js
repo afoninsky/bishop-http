0eb96cd94e409bc7af069aefd49b3f087ddb1a6d
Anfey <vkfont@yandex.ru>
2017-03-01 12:40:04 +0300

    Add timeout tests

--- test/transport.js
+++ test/transport.js
@@ -2,6 +2,7 @@ const { test } = require('ava')
 const Bishop = require('bishop')
 const request = require('request-promise')
 const transport = require(process.env.PWD)
+const Promise = require('bluebird')
 
 test('default answer', async t => {
   const bishop = new Bishop()
@@ -34,7 +35,39 @@ test('client-server interaction', async t => {
   t.is(await bishopClient.act('some:stuff, with:stuff'), 'hello')
 })
 
-test.todo('ensure timeouts are inherited')
+test('ensure timeouts are inherited', async t => {
+  const bishopServer = new Bishop()
+  const bishopClient = new Bishop({
+    timeout: 100 // default timeout
+  })
+  await bishopServer.use(transport, {
+    name: 'http-server',
+    listenPort: 9004,
+  })
+  await bishopClient.use(transport, {
+    timeout: 200, // redefine default timeout
+    name: 'http-client',
+    remote: 'http://localhost:9004',
+    pattern: 'some:stuff'
+  })
+  bishopServer.add('some:stuff', async message => {
+    await Promise.delay(message.delay || 0)
+    return {
+      timeout: parseInt(message.$timeout, 10)
+    }
+  })
+  const res1 = await bishopClient.act('some:stuff')
+  t.is(res1.timeout, 200, 'should rewrite default timeout')
+
+  const res2 = await bishopClient.act('some:stuff, $timeout:201')
+  t.is(res2.timeout, 201, 'should take timeout from query if set')
+
+  const res3 = await bishopClient.act('some:stuff, delay:150')
+  t.is(res3.timeout, 200, 'should use redefined timeout and not throw')
+
+  t.throws(bishopClient.act('some:stuff, delay:210'))
+
+})
 
 test('handle remote error', async t => {
   const bishopServer = new Bishop()
